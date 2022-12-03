65dd3a6fdb4846b9928490bc87fad56a010408ac
Taylor Vercillo <taylornvcasey@gmail.com>
2022-12-03 16:47:16 -0700

    updating links

--- src/components/App.js
+++ src/components/App.js
@@ -12,21 +12,21 @@ function App(){
         {
             id: 1, 
             name: "Greeky Mythology", 
-            description: "An application for the ancient Greece lover! Created with a React frontend and a JSON server backend, this app showcases full C.R.U.D capabilities.",
+            description: "An application for the ancient Greece lover! Created with a React frontend and a Rails backend, this app allows you to add/delete your favorite greek characters and information about them.",
             image: "https://www.realmofhistory.com/wp-content/uploads/2018/05/facts_greek_gods_goddesses_17-min.jpg", 
-            link: "https://greekmythologyapp.netlify.app/"
+            link: "https://greekfrontend.herokuapp.com/"
         }, 
         {
             id: 2, 
             name: "Memory Tracker", 
-            description: "An application to help you hang on to those family memories! Created with a React frontend and a Ruby backend (Sinatra), this app showcases full C.R.U.D capabilities. ",
+            description: "An application to help you hang on to those family memories! Created with a React frontend and a Rails backend, this app showcases full C.R.U.D capabilities. ",
             image: "https://tse1.mm.bing.net/th?id=OIP.G8tBcAWXGRQrMBbimY-VNgHaHa&pid=Api&P=0",
             link: "https://memorytrackerreact.netlify.app/"
         }, 
         {
             id: 3, 
             name: "Chore Board", 
-            description: "An application that keeps your family members informed of their chores. Created with a React frontend and a Ruby on Rails backend, this app showcases full C.R.U.D capabilities. ", 
+            description: "An application that keeps your family members informed of their chores. Created with a React frontend and a Rails backend, this app showcases full C.R.U.D capabilities, authentication, and authorization. ", 
             image: "https://www.superhealthykids.com/wp-content/uploads/2019/08/il_fullxfull.1978948793_112r.jpg", 
             link: "https://familychorechart.netlify.app/"
         }
