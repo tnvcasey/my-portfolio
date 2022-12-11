0788a5f4602dcc6469e21e5430c01e8e0771b253
Taylor Vercillo <taylornvcasey@gmail.com>
2022-12-10 20:48:28 -0700

    pic

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home(){
             <p className="bio">I specialize in React and Ruby applications.</p>
             <p className="bio">I'm dedicated to continuous learning and contributing to the world of tech!</p>
             <p className="bio">Thanks for checking out my portfolio!</p>
-            <img src="https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/318347470_2029479673918922_2634484046568680005_n.jpg?stp=dst-jpg_p526x296&_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=u-P6WC2a6u0AX_7Mxyo&_nc_ht=scontent-den4-1.xx&oh=00_AfCyQmn57oQhJC6vzAJyZztI6UPEXLNSZ8OeMv2agkD37A&oe=639057B5" width="400px" height="500px" className="center" />
+            <img src="https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/318347470_2029479673918922_2634484046568680005_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=VPLUQCQOzloAX_UpYRp&_nc_ht=scontent-den4-1.xx&oh=00_AfAKlpOlDem1EfATkqiZKzRz8O7fTLT6SZt2VgRFUkVzTA&oe=639A3AF5" width="400px" height="500px" className="center" />
         </div>
     )
 }
