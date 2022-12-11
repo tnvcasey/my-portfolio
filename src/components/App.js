4eed548fda34a64bea53bef5af4ed28671e88527
Taylor Vercillo <taylornvcasey@gmail.com>
2022-12-10 20:59:21 -0700

    done

--- src/components/App.js
+++ src/components/App.js
@@ -13,22 +13,22 @@ function App(){
             id: 1, 
             name: "Greeky Mythology", 
             description: "An application for the ancient Greece lover! Created with a React frontend and a Rails backend, this app allows you to add/delete your favorite greek characters and information about them.",
-            image: "https://www.realmofhistory.com/wp-content/uploads/2018/05/facts_greek_gods_goddesses_17-min.jpg", 
+            image: "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/319509687_2035394189994137_3505637945790826824_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=NK0QNkIC8PYAX9HBObF&_nc_ht=scontent-den4-1.xx&oh=00_AfDAlymoLqcS0DSAIGMvZlhK7MdM-XGg6vCM0sNm5eusPw&oe=639A847E", 
             link: "https://greekfrontend.herokuapp.com/"
         }, 
         {
             id: 2, 
             name: "Memory Tracker", 
             description: "An application to help you hang on to those family memories! Created with a React frontend and a Rails backend, this app showcases full C.R.U.D capabilities. ",
-            image: "https://tse1.mm.bing.net/th?id=OIP.G8tBcAWXGRQrMBbimY-VNgHaHa&pid=Api&P=0",
+            image: "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/319502108_2035394359994120_2079325722534895815_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=UlJErvT5T6IAX9hk07e&_nc_ht=scontent-den4-1.xx&oh=00_AfDXurIVk9sED9A-b8HWItu7p38zWl1OVHCNstWXiPR0vw&oe=639A93C0",
             link: "https://pure-earth-70536.herokuapp.com/"
         }, 
         {
             id: 3, 
-            name: "Chore Board", 
-            description: "An application that keeps your family members informed of their chores. Created with a React frontend and a Rails backend, this app showcases full C.R.U.D capabilities, authentication, and authorization. ", 
-            image: "https://www.superhealthykids.com/wp-content/uploads/2019/08/il_fullxfull.1978948793_112r.jpg", 
-            link: "https://secret-island-48967.herokuapp.com/"
+            name: "The Informer", 
+            description: "A real estate app that lets you post the lesser known, but just as important, details before puschasing a home. This app focuses on authentication, and authorization through a Rails backend, and uses a React frontend. ", 
+            image: "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/319447671_2035393966660826_3990185311603833727_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=-qmPv5jCF78AX8sK_d7&_nc_ht=scontent-den4-1.xx&oh=00_AfDhBMCYgJAS5ouZQjdbcTpHfT44Yh7L1Qs3LrRgNQDuNw&oe=639AE7E4", 
+            link: "https://damp-beyond-33023.herokuapp.com/"
         }
     ]
 
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,7 +2,7 @@ function Resume(){
     return (
         <div>
             <h1 className="resume">My Resume</h1>
-            <img src="https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/318146877_2029487107251512_6055621010558348162_n.jpg?stp=dst-jpg_s1080x2048&_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=l1JnU3q4FCgAX9Gd0xj&tn=As0LGwrXI5DHCiuW&_nc_ht=scontent-den4-1.xx&oh=00_AfAmNGDZuxhhUZmd9yQ5sbil2nuBXxfC_sOAwfc4z6boDQ&oe=6391764A" className="res" />
+            <img src="https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/318146877_2029487107251512_6055621010558348162_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=DWAbjmz-YHQAX9xo7Fh&tn=As0LGwrXI5DHCiuW&_nc_ht=scontent-den4-1.xx&oh=00_AfBZkWasY3JLz_mP9yqKTmefBUJv4Wv-1Ks835YyIEEWfg&oe=63995F4A" className="res" />
             </div>
     )
 
