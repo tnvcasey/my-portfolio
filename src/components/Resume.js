a2b706b7df07c575c1b93a7cfba1acd0f7db6cd1
Taylor Vercillo <taylornvcasey@gmail.com>
2022-12-17 16:36:56 -0700

    updated pic urls

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,7 +2,7 @@ function Resume(){
     return (
         <div>
             <h1 className="resume">My Resume</h1>
-            <img src="https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/318146877_2029487107251512_6055621010558348162_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=DWAbjmz-YHQAX9xo7Fh&tn=As0LGwrXI5DHCiuW&_nc_ht=scontent-den4-1.xx&oh=00_AfBZkWasY3JLz_mP9yqKTmefBUJv4Wv-1Ks835YyIEEWfg&oe=63995F4A" className="res" />
+            <img src="https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/318146877_2029487107251512_6055621010558348162_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=br-ZFw0Bec0AX8s8j1b&tn=As0LGwrXI5DHCiuW&_nc_ht=scontent-den4-1.xx&oh=00_AfDNwJpb-nYCmYohYALnMAhElfX146qCkTqjzKZ1esj5zA&oe=63A3428A" className="res" />
             </div>
     )
 
