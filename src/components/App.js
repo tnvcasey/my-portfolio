1920e2166050bd74f94438949626ebec9ab29b1a
Taylor Vercillo <taylornvcasey@gmail.com>
2022-12-17 16:39:52 -0700

    added js

--- src/components/App.js
+++ src/components/App.js
@@ -12,21 +12,21 @@ function App(){
         {
             id: 1, 
             name: "Greek Mythology", 
-            description: "An application for the ancient Greece lover! Created with a React frontend and a Rails backend, this app allows you to add/delete your favorite greek characters and information about them.",
+            description: "An application for the ancient Greece lover! Created with a React JS frontend and a Rails backend, this app allows you to add/delete your favorite greek characters and information about them.",
             image: "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/319509687_2035394189994137_3505637945790826824_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=DwNMKg5oqpIAX_y09ze&_nc_ht=scontent-den4-1.xx&oh=00_AfDYSrQj-MS-9TnDnW21f3itEaqZMcXSzezByU-UtHq4jg&oe=63A26D7E", 
             link: "https://greekfrontend.herokuapp.com/"
         }, 
         {
             id: 2, 
             name: "Memory Tracker", 
-            description: "An application to help you hang on to those family memories! Created with a React frontend and a Rails backend, this app showcases full C.R.U.D capabilities. ",
+            description: "An application to help you hang on to those family memories! Created with a React JS frontend and a Rails backend, this app showcases full C.R.U.D capabilities. ",
             image: "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/319502108_2035394359994120_2079325722534895815_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=wii97nHs4wsAX-wHsbH&_nc_ht=scontent-den4-1.xx&oh=00_AfDH2GVEgCi5DHe35Gzfn29aphr1Pj8PvYpJN9er-WSLMQ&oe=63A27CC0",
             link: "https://pure-earth-70536.herokuapp.com/"
         }, 
         {
             id: 3, 
             name: "The Informer", 
-            description: "A real estate app that lets you post the lesser known, but just as important, details before puschasing a home. This app focuses on authentication, and authorization through a Rails backend, and uses a React frontend. ", 
+            description: "A real estate app that lets you post the lesser known, but just as important, details before puschasing a home. This app focuses on authentication, and authorization through a Rails backend, and uses a React JS frontend. ", 
             image: "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/319447671_2035393966660826_3990185311603833727_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=eufGVH7K4QAAX8MWN55&_nc_ht=scontent-den4-1.xx&oh=00_AfDD0HSOdp00C_yJQvhLp1CUS138RZeZrwDTiwiI2uCYYA&oe=63A2D0E4", 
             link: "https://damp-beyond-33023.herokuapp.com/"
         }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home(){
             <h1 className="intro">Hi, my name is Taylor!</h1>
             <h2 className="explain">Full Stack Developer | Mom | Diet Coke Enthusiast</h2>
             <p className="bio">I'm a full stack developer newly graduated from Flatiron School.</p>
-            <p className="bio">I specialize in React and Ruby applications.</p>
+            <p className="bio">I specialize in JavaScript and Ruby applications.</p>
             <p className="bio">I'm dedicated to continuous learning and contributing to the world of tech!</p>
             <p className="bio">Thanks for checking out my portfolio!</p>
             <img src="https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/318347470_2029479673918922_2634484046568680005_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=a7JtX0AMwJAAX8WNDVM&_nc_ht=scontent-den4-1.xx&oh=00_AfC4oOlP2Bz1V5UvMe1RdjIKFcgaFgQZJcdxWzPbdLHnug&oe=63A41E35" width="400px" height="500px" className="center" />
