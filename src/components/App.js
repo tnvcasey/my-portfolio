2c8619a81a4f51aeefc5c2ce7b708909fd010345
Taylor Vercillo <taylornvcasey@gmail.com>
2023-01-01 15:46:34 -0700

    pics

--- src/components/App.js
+++ src/components/App.js
@@ -13,28 +13,28 @@ function App(){
             id: 1, 
             name: "Greek Mythology", 
             description: "An application for the ancient Greece lover! Created with a React JS frontend and a Rails backend, this app allows you to add/delete your favorite greek characters and information about them.",
-            image: "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/319509687_2035394189994137_3505637945790826824_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=lCaOOSwsJWoAX_u8pcW&_nc_ht=scontent-den4-1.xx&oh=00_AfBH7PmM458bl6SYL8rASeVDQ9haj532Sl6a5vuY_cd6hg&oe=63AA567E", 
+            image: "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/319509687_2035394189994137_3505637945790826824_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=ydvvJLWIQ2QAX8iAHj6&_nc_ht=scontent-den4-1.xx&oh=00_AfCqgE3rqfUKD2zkSYeVH6DZmKuiEdgosPrytiCZvi5LNQ&oe=63B633FE", 
             link: "https://greekfrontend.herokuapp.com/"
         }, 
         {
             id: 2, 
             name: "Memory Tracker", 
             description: "An application to help you hang on to those family memories! Created with a React JS frontend and a Rails backend, this app showcases full C.R.U.D capabilities. ",
-            image: "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/320888289_1838470959833868_5277504167270404134_n.jpg?stp=cp6_dst-jpg&_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=XhHTPoFNPB4AX_AxF1q&_nc_ht=scontent-den4-1.xx&oh=00_AfD7202g3HaBK0AX74dI0ASYqMmklg8-_nuDYWaO1Mcdqw&oe=63AA9DB9",
+            image: "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/320888289_1838470959833868_5277504167270404134_n.jpg?stp=cp6_dst-jpg&_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=pQIFOLokJGsAX-CdrcH&_nc_ht=scontent-den4-1.xx&oh=00_AfA3C9r3iCpl3DI_cdrLctNiDjctBqklSQSwa-nfTaMFdg&oe=63B67B39",
             link: "https://pure-earth-70536.herokuapp.com/"
         }, 
         {
             id: 3, 
             name: "The Informer", 
             description: "A real estate app that lets you post the lesser known, but just as important, details before puschasing a home. This app focuses on authentication, and authorization through a Rails backend, and uses a React JS frontend. ", 
-            image: "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/320059980_673197777684409_1140011154830140851_n.jpg?stp=cp6_dst-jpg&_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=9HgV8RFK4rAAX88M5bP&_nc_ht=scontent-den4-1.xx&oh=00_AfBiradtF1EXGubny4IC2TpShbsJpsz1CkuET1RUQs5NNg&oe=63ABC946", 
+            image: "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/320059980_673197777684409_1140011154830140851_n.jpg?stp=cp6_dst-jpg&_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=PaRH45PvJ5wAX9yYePh&_nc_ht=scontent-den4-1.xx&oh=00_AfB3wRllQ0ZbaXGWny9-JzYT9SACGfTSUM70uoaGwVMoGA&oe=63B7A6C6", 
             link: "https://damp-beyond-33023.herokuapp.com/"
         },
         {
             id: 4, 
             name: "Sam's Sweet Shop", 
             description: "A simple sweet shop app dislpaying your favorite sweets! Created with a React JS frontend and a Rails backend.",
-            image: "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/320173013_514633007396705_3079855972047229465_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=I5mtb8a40lQAX-gf4vq&_nc_ht=scontent-den4-1.xx&oh=00_AfAeJxtfjmdAFx1kq0Q9RruUgIrZW5YGZ0ywfJb6vd0Ajg&oe=63AABF91",
+            image: "https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/320173013_514633007396705_3079855972047229465_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=z_MpIUwogJIAX8PNxhG&_nc_ht=scontent-den4-1.xx&oh=00_AfD_9i5W07aLSKYQ02rGYW8EX5mTzor4BP1AGRtrCla9Sw&oe=63B69D11",
             link: "https://powerful-forest-01461.herokuapp.com/"
         }
     ]
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,7 +2,7 @@ function Resume(){
     return (
         <div>
             <h1 className="resume">My Resume</h1>
-            <img src="https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/318146877_2029487107251512_6055621010558348162_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=Po90xiNESREAX__fIBp&tn=As0LGwrXI5DHCiuW&_nc_ht=scontent-den4-1.xx&oh=00_AfCWQcrLSVbKEIkvB5jr_L9S_E3w6vyrZGka44YjMBusxQ&oe=63AB2B8A" className="res" />
+            <img src="https://scontent-den4-1.xx.fbcdn.net/v/t39.30808-6/318146877_2029487107251512_6055621010558348162_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=FGN6R2itA9QAX8JVmPM&tn=As0LGwrXI5DHCiuW&_nc_ht=scontent-den4-1.xx&oh=00_AfCXN2efYRRzqmoiDAL2Hek1npO1COSn-RWN3b82MBI3cQ&oe=63B7090A" className="res" />
             </div>
     )
 
